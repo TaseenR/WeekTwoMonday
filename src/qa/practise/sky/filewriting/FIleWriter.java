3094630ad9c5d5152a315093defb256b3a272baa
Taz <taseen156@gmail.com>
2023-07-28 11:47:32 +0100

    Reading file done

---
+++ src/qa/practise/sky/filewriting/FIleWriter.java
@@ -0,0 +1,21 @@
+package qa.practise.sky.filewriting;
+import java.io.*;
+import java.util.Scanner;
+
+public class FIleWriter {
+
+    public static void read(){
+        try{
+            File poem = new File("/Users/trm08/Desktop/SkyCamp/SkyWeekTwoMonday/src/qa/practise/sky/filewriting/poem");
+            Scanner scanner = new Scanner(poem);
+            while (scanner.hasNextLine()){
+                String data = scanner.nextLine();
+                System.out.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/qa/practise/sky/filewriting/Runner.java
@@ -0,0 +1,10 @@
+package qa.practise.sky.filewriting;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        FIleWriter reader = new FIleWriter();
+        reader.read();
+    }
+
+}
